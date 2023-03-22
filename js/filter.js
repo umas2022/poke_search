21bdd5e31b49152f8a25a4b58577ef03179139b4
umas <1970313791@qq.com>
2023-03-22 14:34:16 +0100

    add option

--- js/filter.js
+++ js/filter.js
@@ -1,10 +1,31 @@
 
 /**
- * 指定敌方属性
+ * 所有选中的选项
  */
-var enemy_type = {
-    type_1: "",
-    type_2: "",
-    type_3: "",
-    type_4: "",
-} 
+var selected = {
+    // 指定敌方属性
+    enemy_type: {
+        type_1: "",
+        type_2: "",
+        type_3: "",
+        type_4: "",
+    },
+    // 指定我方属性
+    my_type: {
+        type_1: "",
+        type_2: ""
+    },
+    //指定我方技能
+    skill: {
+        skill_1: "",
+        skill_2: "",
+        skill_3: "",
+        skill_4: "",
+    },
+    // 指定伤害倍率
+    multi:{
+        atk:"",
+        dfs:""
+    }
+}
+
